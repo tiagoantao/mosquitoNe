be2b5cfe0a3bb622d5c675603cd2534a4f54a6ff
Tiago Antao <tiagoantao@gmail.com>
2013-04-26 09:12:54 +0100

    Genepop Concatenation

---
+++ src/concatenateGP.py
@@ -0,0 +1,34 @@
+import sys
+
+import myUtils
+
+model = sys.argv[1]
+
+
+cfg = myUtils.getConfig(model)
+popSize = cfg.popSize
+blocks = myUtils.getBlocks(cfg)
+
+for numIndivs, numLoci in cfg.sampleStrats:
+    for rep in range(cfg.reps):
+        for i in range(len(blocks)):
+            wname = myUtils.getConc(cfg, numIndivs, numLoci, rep)
+            w = open(wname + "-" + str(i), "w")
+            onFirst = True
+            w.write("conc\n")
+            gens = set(blocks[i])
+            gens = list(gens)
+            gens.sort()
+            for gen in gens:
+                name = myUtils.getExpr(cfg, numIndivs, numLoci, gen, rep, True)
+                f = open(name)
+                sendOut = False
+                f.readline()
+                for l in f:
+                    if sendOut or onFirst:
+                        w.write(l)
+                    elif l.startswith("Pop"):
+                        w.write(l)
+                        sendOut = True
+                onFirst = False
+            w.close()
