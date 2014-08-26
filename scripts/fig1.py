df2cf2b29bc91ae295fdd41ad43157f37cdf7991
Tiago Antao <tiagoantao@gmail.com>
2014-08-26 19:34:09 +0100

    figure1

--- scripts/fig1.py
+++ scripts/fig1.py
@@ -1,43 +1,61 @@
-import pylab
 import sys
 import ConfigParser
+
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+
+import myUtils
 
 if len(sys.argv) not in [2]:
     print "syntax: %s <plotconfig>" % sys.argv[0]
     sys.exit(-1)
 
+
 def getPlotConfig(fName):
     config = ConfigParser.ConfigParser()
     config.read(fName)
-    return (eval(config.get("params","nc")), eval(config.get("params","span")),
-            eval(config.get("params","sampleStrat")))
+    return (eval(config.get("params", "nc")),
+            eval(config.get("params", "span")),
+            eval(config.get("params", "sampleStrat")))
 
 
 ncs, spans, sampleStrats = getPlotConfig(sys.argv[1])
 numCols = len(ncs)
 numRows = len(spans)
 
-def doPlot(nc, span, startCol, endRow):
+
+def doPlot(ax, nc, span, startCol, endRow):
     sampRes = []
+    cfg = myUtils.getConfig('simple%d' % nc)
     for numIndivs, numLoci in sampleStrats:
+        print numIndivs, numLoci
         sampRes.append([])
-        sampRes[(numIndivs, numLoci)] = []
         #we assume that t0 is file(gen[0])-1
-        f = open("NeF-smp-%d-%d-%d.txt" % (numIndivs, numLoci, nc))
-        #we assume span>1
-        fileStartGen = int(f.readline().split(" ")[0])
+        f = open(myUtils.getStatName(cfg, numIndivs, numLoci))
+        l = f.readline()
+        while l != "":
+            if l.find('temp') > -1:
+                break
+            l = f.readline()
+        f.readline()  # pcrits
         l = f.readline()
         while l != "":
             toks = l.rstrip().split(" ")
-            gen = int(toks[0])
-            if gen - fileStartGen + 1 == span:
-                if toks[2]=="NeF":
-                    sampRes[-1].append(float(toks[3]))
+            rep = int(toks[0])
+            ref = int(toks[1])
+            gen = int(toks[2])
+            if gen - ref != span:
+                l = f.readline()
+                continue
+            stat = toks[6].split('#')  # Pollak 0.01
+            sampRes[-1].append(float(stat[1]))
             l = f.readline()
-    pylab.boxplot(sampRes)
+    ax.boxplot(sampRes)
 
-
-for col in range(ncs):
-    for row in range(spans):
-        pylab.subplot(numRows, numCols, col*numRows + row + 1)
-        doPlot(nc[col],spans[row], col==0, row==numRows)
+plt.ioff()
+for col in range(len(ncs)):
+    for row in range(len(spans)):
+        ax = plt.subplot(numRows, numCols, col * numRows + row + 1)
+        doPlot(ax, ncs[col], spans[row], col == 0, row == numRows)
+plt.savefig('fig1.png')
