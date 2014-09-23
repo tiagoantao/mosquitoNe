cd4d57d5bb333959d8fd5ad24e063390d6f1dbe9
Tiago Antao <tiagoantao@gmail.com>
2014-09-23 05:20:19 +0100

    decline

--- scripts/figDecline.py
+++ scripts/figDecline.py
@@ -2,17 +2,18 @@ from collections import defaultdict
 #import sys
 #import ConfigParser
 
+import numpy as np
+from scipy import stats
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
-from scipy import stats
 
 import myUtils
 from myUtils import flt
 
 ncs = [5000, 2000, 1000]
 numIndivs = 60
-numLoci = 10
+numLoci = 20
 
 
 def doCI(ax, nc, last_row):
@@ -56,7 +57,7 @@ def doCI(ax, nc, last_row):
             l = f.readline()
             continue
         gen = int(toks[2])
-        if gen not in [cfg.declineGen - 1, cfg.declineGen,
+        if gen not in [cfg.declineGen, cfg.declineGen + 1,
                        cfg.declineGen + 10]:
             l = f.readline()
             continue
@@ -66,13 +67,20 @@ def doCI(ax, nc, last_row):
     bp_ld = []
     for i in range(len(LD)):
         p = 0 if i == 0 else 1
-        bp_ld.append([stats.hmean([y[p] if y[p] > 0 else 100000
-                                   for y in LD[i]])])
+        bp_ld.append([np.median([y[p] if y[p] > 0 else 100000
+                                 for y in LD[i]])])
     ax.boxplot(bp_ld, sym='')
+    ax.set_ylim(0, nc // 5)
 
 
 def doPlot(ax, nc, last_row):
     cfg = myUtils.getConfig('decl-%d-%d' % (nc, nc // 10))
+    if nc == 1000:
+        mdecl = defaultdict(dict)
+        for rep, ref, gen, vals in myUtils.getMLNE(cfg, numIndivs, numLoci):
+            top, point, bot = vals
+            mdecl[ref].setdefault(gen, [])
+            mdecl[ref][gen].append(point)
     f = open(myUtils.getStatName(cfg, numIndivs, numLoci))
     l = f.readline()
     LD = defaultdict(list)
@@ -83,7 +91,7 @@ def doPlot(ax, nc, last_row):
         rep, gen = l.rstrip().split(' ')
         rep = int(rep)
         gen = int(gen)
-        if gen < cfg.declineGen + 1:
+        if gen < cfg.declineGen:
             l = f.readline()
             l = f.readline()
             l = f.readline()
@@ -109,27 +117,33 @@ def doPlot(ax, nc, last_row):
         ref = int(toks[1])
         tdecl.setdefault(ref, {})
         gen = int(toks[2])
-        if gen < cfg.declineGen + 1:
+        if gen < cfg.declineGen:
             l = f.readline()
             continue
         tdecl[ref].setdefault(gen, [])
         stat = toks[-1].split('#')  # Nei/Tajima 0+
         tdecl[ref][gen].append(flt(stat[1]))
         l = f.readline()
-    ax.plot([stats.hmean([y if y > 0 else 100000 for y in LD[gen]])
+    ax.plot([np.median([y if y > 0 else 100000 for y in LD[gen]])
              for gen in LD], label='LD')
     for ref, lst in tdecl.items():
-        ax.plot([stats.hmean([y if y > 0 else 100000 for y in lst[gen]])
+        ax.plot([np.median([y if y > 0 else 100000 for y in lst[gen]])
                  for gen in lst], label=str(ref))
+    if nc == 1000:
+        for ref, lst in mdecl.items():
+            ax.plot([np.median([y if y > 0 else 100000 for y in lst[gen]])
+                    for gen in lst], label='m ' + str(ref))
     ax.set_xlim(0, 20)
+    ax.set_ylim(0, nc)
     ax.legend()
 
 plt.ioff()
 numRows = len(ncs)
-fig, axs = plt.subplots(numRows, 2, sharex=True, figsize=(16, 9))
+fig, axs = plt.subplots(numRows, 1, sharex=True, figsize=(16, 9),
+                        squeeze=False)
 for row in range(numRows):
     ax = axs[row, 0]
     doPlot(ax, ncs[row], row == numRows - 1)
-    ax = axs[row, 1]
-    doCI(ax, ncs[row], row == numRows - 1)
+    #ax = axs[row, 1]
+    #doCI(ax, ncs[row], row == numRows - 1)
 plt.savefig('decline.png')
